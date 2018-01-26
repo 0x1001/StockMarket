352937686c046d11b6ae87eb604c4f0871f1a8dc
0x1001 <damian.nowok@gmail.com>
2018-01-26 23:41:14 +0100

    Added install script

---
+++ crypto/install.bat
@@ -0,0 +1,17 @@
+set PYTHON=c:/tools/Python3.5
+
+IF EXIST ".venv" (
+    rmdir /q /s .venv
+)
+
+%PYTHON%/python.exe -m venv .venv
+
+IF NOT EXIST ".venv" (
+	echo Python virtual environment faild to create.
+	pause
+	exit 1
+)
+
+call .venv\scripts\activate.bat
+pip install https://github.com/s4w3d0ff/python-poloniex/archive/v0.4.7.zip
+pip install -r requirements.txt
