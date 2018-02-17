9d3988482c1ebd755cb919005ce77647aa55b970
0x1001 <damian.nowok@gmail.com>
2018-02-17 14:35:54 +0100

    Two hidden layers

--- crypto/neural_network.py
+++ crypto/neural_network.py
@@ -25,13 +25,7 @@ def neural_network_model(data):
     hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
                       'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
-    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
-
-    hidden_4_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_nodes_hl4])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl4]))}
-
-    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl4, n_classes])),
+    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_classes])),
                     'biases': tf.Variable(tf.random_normal([n_classes])), }
 
     data_norm = tf.nn.l2_normalize(data)
@@ -42,13 +36,7 @@ def neural_network_model(data):
     l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
     l2 = tf.nn.relu(l2)
 
-    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
-    l3 = tf.nn.relu(l3)
-
-    l4 = tf.add(tf.matmul(l3, hidden_4_layer['weights']), hidden_4_layer['biases'])
-    l4 = tf.nn.relu(l4)
-
-    output = tf.matmul(l4, output_layer['weights']) + output_layer['biases']
+    output = tf.matmul(l2, output_layer['weights']) + output_layer['biases']
 
     return output
 
@@ -164,4 +152,8 @@ def one_hot_encoding(data):
 
 
 if __name__ == "__main__":
+    import datetime
+
+    start = datetime.datetime.now()
     train_neural_network(x)
+    print("Execution time:",(datetime.datetime.now() - start).total_seconds(), "Seconds")
